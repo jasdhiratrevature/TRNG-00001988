1599d1bf54b421e5df62887ef6649df113f1f69f
Jasdhir Singh <jasdhir.singh@revature.com>
2024-08-02 14:51:06 +0530

    Logback demo

--- 080224/FileHandling/src/main/java/com/revature/files/serial/EmployeeSerialize.java
+++ 080224/FileHandling/src/main/java/com/revature/files/serial/EmployeeSerialize.java
@@ -7,9 +7,16 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 public class EmployeeSerialize {
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeSerialize.class);
 public static void main(String[] args) {
 	
+	 
 	
 	FileOutputStream fos=null;
 	ObjectOutputStream oos;
@@ -17,10 +24,19 @@ public static void main(String[] args) {
 	Employee emp=null;
 	// Write Employee Object to a file
 	try {
+		logger.trace("Trace");
+		logger.debug("Debug");
+		logger.info("Info");
+		logger.warn("Warn");
+		logger.error("Error");
+		logger.debug("Opening file for writing Object");
 		fos=new FileOutputStream("C:\\filehandling\\employee.dat");
 		oos=new ObjectOutputStream(fos);
+		logger.info("Creating Object");
 		emp=new Employee(101, "Jasdhir", "Trainer");
+		logger.info("Writing Object ....");
 		oos.writeObject(emp);
+		logger.info("Object written to file -- Serialized");
 	} catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
